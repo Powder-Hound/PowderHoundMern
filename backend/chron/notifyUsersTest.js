30d75b9d36f2251bf65f4c411f796b187776b244
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-12-14 14:46:20 -0800

    tech debt

--- backend/chron/notifyUsersTest.js
+++
@@ -1,8 +0,0 @@
-import { sendTextMessage } from "../__mocks__/twilioMiddleware.js";
-import client from "../__mocks__/postmarkMiddleware.js";
-
-// Replace other imports and logic as needed
-export const checkResorts = async () => {
-  console.log("Running mocked refactored checkResorts...");
-  // Insert your logic here (no change)
-};
